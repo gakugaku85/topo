09b32f461ad6a8a34417122d826d04f1a827f211
sekiguchi gaku <s238319q@st.go.tuat.ac.jp>
2023-10-12 11:13:21 +0900

    matchingを導入した

--- gudhi_distance.py
+++ gudhi_distance.py
@@ -5,13 +5,12 @@ from PIL import Image
 
 from gudhi.wasserstein import wasserstein_distance
 
-a = np.array([[10.0, 18.0], [5.0, 17.0], [1.0, 15.0], [2.0, 14.0]])
-b = np.array([[10.0, 20.0], [5.0, 14.0], [3.0, 13.0]])
+# a = np.array([[10.0, 18.0], [5.0, 17.0], [1.0, 15.0], [2.0, 14.0]])
+# b = np.array([[10.0, 20.0], [5.0, 14.0], [3.0, 13.0]])
 
-message = "Bottleneck distance = " + "%.1f" % gd.bottleneck_distance(
-    [[0.0, 0.0], [0.0, 5.0]], [[0.0, 13.0], [0.0, 16.0]]
-)
-print(message)
+b = np.array([[10.0, 18.0], [5.0, 17.0], [1.0, 15.0], [2.0, 13.0]])
+a = np.array([[10.0, 14.0], [1.0, 13.0], [3.0, 13.0], [5.0, 14.0], [10.0, 20.0]])
+
 
 # plot_diagram(a,b)
 plt.clf()
@@ -22,34 +21,52 @@ plt.legend(bbox_to_anchor=(1, 0), loc="lower right", borderaxespad=1)
 plt.xlabel("Birth")
 plt.ylabel("Death")
 plt.plot([0, 20], [0, 20], c="r")  # 対角線
-if len(a) >= len(b):
-    for i in range(len(a)):
-        if len(b) > i:
-            plt.plot(
-                [a[i, 0], b[i, 0]], [a[i, 1], b[i, 1]], linestyle="--", c="gray"
-            )
-        else:
-            plt.plot(
-                [a[i, 0], a[i, 0]], [a[i, 1], a[i, 0]], linestyle="--", c="gray"
-            )  # 垂直な線
-
-if len(b) > len(a):
-    for i in range(len(b)):
-        if len(a) > i:
-            plt.plot(
-                [a[i, 0], b[i, 0]], [a[i, 1], b[i, 1]], linestyle="--", c="gray"
-            )
-        else:
-            plt.plot(
-                [b[i, 0], b[i, 0]], [b[i, 1], b[i, 0]], linestyle="--", c="gray"
-            )
 
 bottleneck_distance = gd.bottleneck_distance(a, b)
-wasserstein_distance = wasserstein_distance(np.array(a), np.array(b))
+cost, matchings = wasserstein_distance(a, b, matching=True)
+wasserstein_dis = wasserstein_distance(a, b)
 message = "Bottleneck distance = " + "%.3f" % bottleneck_distance
 plt.text(15, 5, message, ha="center", fontsize=10, color="black")
-message = "Wasserstein distance = " + "%.3f" % wasserstein_distance
+message = "Wasserstein distance = " + "%.3f" % cost
 plt.text(15, 4, message, ha="center", fontsize=10, color="black")
+
+for match in matchings:
+    if match[0] != -1 and match[1] != -1:
+        plt.plot(
+            [a[match[0], 0], b[match[1], 0]],
+            [a[match[0], 1], b[match[1], 1]],
+            linestyle="--",
+            c="gray",
+        )
+    else:
+        print("innlo")
+        if match[0] == -1:
+            plt.plot(
+                [b[match[1], 0], b[match[1], 0]],
+                [b[match[1], 1], b[match[1], 0]],
+                linestyle="--",
+                c="gray",
+            )
+        else:
+            plt.plot(
+                [a[match[0], 0], a[match[0], 0]],
+                [a[match[0], 1], a[match[0], 0]],
+                linestyle="--",
+                c="gray",
+            )
 
 plt.savefig("test_result/" + "ab" + "_diagram.png")
 plt.clf()
+
+print(f"Wasserstein distance value = {cost:.2f}")
+print(f"matchings = {matchings}")
+dgm1_to_diagonal = matchings[matchings[:, 1] == -1, 0]
+dgm2_to_diagonal = matchings[matchings[:, 0] == -1, 1]
+off_diagonal_match = np.delete(matchings, np.where(matchings == -1)[0], axis=0)
+
+for i, j in off_diagonal_match:
+    print(f"point {i} in dgm1 is matched to point {j} in dgm2")
+for i in dgm1_to_diagonal:
+    print(f"point {i} in dgm1 is matched to the diagonal")
+for j in dgm2_to_diagonal:
+    print(f"point {j} in dgm2 is matched to the diagonal")
