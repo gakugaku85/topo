64764f573c2a6da253f6f1c893d1d4de27eeec22
sekiguchi gaku <s238319q@st.go.tuat.ac.jp>
2023-10-13 17:39:09 +0900

    とりあえず今日は帰る 先輩のutilsみてもう少し直したほうがいい

--- gudhi_distance.py
+++ gudhi_distance.py
@@ -5,101 +5,87 @@ from PIL import Image
 
 from gudhi.wasserstein import wasserstein_distance
 
-# a = np.array([[10.0, 18.0], [5.0, 17.0], [1.0, 15.0], [2.0, 14.0]])
-# b = np.array([[10.0, 20.0], [5.0, 14.0], [3.0, 13.0]])
-
-a = np.array([[10.0, 18.0], [5.0, 17.0]])
+a = np.array([[10.0, 18.0]])
 b = np.array([[10.0, 14.0], [1.0, 11.0], [3.0, 12.0]])
 
 
-# plot_diagram(a,b)
-plt.clf()
-plt.scatter(a[:, 0], a[:, 1], c="red", label="a")
-plt.scatter(b[:, 0], b[:, 1], c="blue", label="b")
-plt.title("Persistence Diagram")
-plt.legend(bbox_to_anchor=(1, 0), loc="lower right", borderaxespad=1)
-plt.xlabel("Birth")
-plt.ylabel("Death")
-plt.plot([0, 20], [0, 20], c="r")  # 対角線
+def plot_diagram(a, b):
+    plt.clf()
+    plt.scatter(a[:, 0], a[:, 1], c="red", label="a")
+    plt.scatter(b[:, 0], b[:, 1], c="blue", label="b")
+    plt.title("Persistence Diagram")
+    plt.legend(bbox_to_anchor=(1, 0), loc="lower right", borderaxespad=1)
+    plt.xlabel("Birth")
+    plt.ylabel("Death")
+    plt.plot([0, 20], [0, 20], c="r")  # 対角線
 
-bottleneck_distance = gd.bottleneck_distance(a, b)
-cost, matchings = wasserstein_distance(a, b, matching=True)
-wasserstein_dis = wasserstein_distance(a, b)
-message = "Bottleneck distance = " + "%.3f" % bottleneck_distance
-plt.text(15, 5, message, ha="center", fontsize=10, color="black")
-message = "Wasserstein distance = " + "%.3f" % cost
-plt.text(15, 4, message, ha="center", fontsize=10, color="black")
+    bottleneck_distance = gd.bottleneck_distance(a, b)
+    cost, matchings = wasserstein_distance(a, b, matching=True)
+    message = "Bottleneck distance = " + "%.3f" % bottleneck_distance
+    plt.text(15, 5, message, ha="center", fontsize=10, color="black")
+    message = "Wasserstein distance = " + "%.3f" % cost
+    plt.text(15, 4, message, ha="center", fontsize=10, color="black")
 
-for i in range(len(a)):
-    plt.text(
-        a[i, 0] + 0.2,
-        a[i, 1],
-        f"({a[i, 0]:.1f}, {a[i, 1]:.1f})",
-        fontsize=8,
-        color="black",
-    )
-for i in range(len(b)):
-    plt.text(
-        b[i, 0] + 0.2,
-        b[i, 1],
-        f"({b[i, 0]:.1f}, {b[i, 1]:.1f})",
-        fontsize=8,
-        color="black",
-    )
-
-for match in matchings:
-    if match[0] != -1 and match[1] != -1:
-        plt.plot(
-            [a[match[0], 0], b[match[1], 0]],
-            [a[match[0], 1], b[match[1], 1]],
-            linestyle="--",
-            c="gray",
+    for i in range(len(a)):
+        plt.text(
+            a[i, 0] + 0.2,
+            a[i, 1],
+            f"({a[i, 0]:.1f}, {a[i, 1]:.1f})",
+            fontsize=8,
+            color="black",
         )
-    else:
-        if match[0] == -1:
-            x = (b[match[1], 0] + b[match[1], 1]) / 2
-            y = -x + b[match[1], 0] + b[match[1], 1]
+    for i in range(len(b)):
+        plt.text(
+            b[i, 0] + 0.2,
+            b[i, 1],
+            f"({b[i, 0]:.1f}, {b[i, 1]:.1f})",
+            fontsize=8,
+            color="black",
+        )
+
+    for match in matchings:
+        if match[0] != -1 and match[1] != -1:
             plt.plot(
-                [(b[match[1], 0]), x],
-                [b[match[1], 1], y],
+                [a[match[0], 0], b[match[1], 0]],
+                [a[match[0], 1], b[match[1], 1]],
                 linestyle="--",
                 c="gray",
-            )
-            plt.text(
-                x + 0.2,
-                y,
-                f"({x:.1f}, {y:.1f})",
-                fontsize=8,
-                color="black",
             )
         else:
-            x = (a[match[0], 0] + a[match[0], 1]) / 2
-            y = -x + a[match[0], 0] + a[match[0], 1]
-            plt.plot(
-                [a[match[0], 0], x],
-                [a[match[0], 1], y],
-                linestyle="--",
-                c="gray",
-            )
-            plt.text(
-                x + 0.2,
-                y,
-                f"({x:.1f}, {y:.1f})",
-                fontsize=8,
-                color="black",
-            )
+            if match[0] == -1:
+                x = (b[match[1], 0] + b[match[1], 1]) / 2
+                y = -x + b[match[1], 0] + b[match[1], 1]
+                plt.plot(
+                    [(b[match[1], 0]), x],
+                    [b[match[1], 1], y],
+                    linestyle="--",
+                    c="gray",
+                )
+                plt.text(
+                    x + 0.2,
+                    y,
+                    f"({x:.1f}, {y:.1f})",
+                    fontsize=8,
+                    color="black",
+                )
+            else:
+                x = (a[match[0], 0] + a[match[0], 1]) / 2
+                y = -x + a[match[0], 0] + a[match[0], 1]
+                plt.plot(
+                    [a[match[0], 0], x],
+                    [a[match[0], 1], y],
+                    linestyle="--",
+                    c="gray",
+                )
+                plt.text(
+                    x + 0.2,
+                    y,
+                    f"({x:.1f}, {y:.1f})",
+                    fontsize=8,
+                    color="black",
+                )
+    plt.savefig("test_result/" + "ab" + "_diagram.png")
+    plt.clf()
 
-plt.savefig("test_result/" + "ab" + "_diagram.png")
-plt.clf()
 
-print(f"Wasserstein distance value = {cost:.2f}")
-dgm1_to_diagonal = matchings[matchings[:, 1] == -1, 0]
-dgm2_to_diagonal = matchings[matchings[:, 0] == -1, 1]
-off_diagonal_match = np.delete(matchings, np.where(matchings == -1)[0], axis=0)
-
-for i, j in off_diagonal_match:
-    print(f"point {i} in dgm1 is matched to point {j} in dgm2")
-for i in dgm1_to_diagonal:
-    print(f"point {i} in dgm1 is matched to the diagonal")
-for j in dgm2_to_diagonal:
-    print(f"point {j} in dgm2 is matched to the diagonal")
+plot_diagram(a, b)
--- updated_gudhi_persistence.py
+++ updated_gudhi_persistence.py
@@ -5,6 +5,8 @@ from PIL import Image
 
 from gudhi.wasserstein import wasserstein_distance
 
+from gudhi_distance import plot_diagram
+
 # Turn off usetex mode to avoid requiring TeX
 plt.rcParams["text.usetex"] = False
 
@@ -22,12 +24,27 @@ plt.rcParams["text.usetex"] = False
 
 image_array = np.array(
     [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 0],
-        [0, 0.4, 0, 0, 0.2, 0],
-        [0, 1, 0, 0, 1, 0],
-        [0, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0, 0.5, 0.5, 0, 1, 0],
+        [0, 1, 0, 0.5, 0.5, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+)
+
+image_array_2 = np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 0.3, 0.5, 0.5, 0, 1, 0],
+        [0, 1, 0, 0.5, 0.5, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
     ]
 )
 
@@ -41,17 +58,6 @@ image_array = np.array(
 #         [1, 1, 1, 1, 1, 1],
 #     ]
 # )
-
-image_array_2 = np.array(
-    [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 0],
-        [0, 0.8, 0, 0, 0.3, 0],
-        [0, 1, 0, 0, 1, 0],
-        [0, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0],
-    ]
-)
 
 
 def plot_persistence_diagram(persistence, output_file_name="output"):
@@ -67,7 +73,11 @@ def plot_persistence_diagram(persistence, output_file_name="output"):
 def plot_persistence_barcode(persistence, output_file_name="output"):
     """Plots the persistence barcode."""
     plt.clf()
-    gd.plot_persistence_barcode(persistence)
+    gd.plot_persistence_barcode(persistence, max_intervals=0, inf_delta=100)
+    plt.xlim(0, 1)
+    plt.ylim(-1, len(persistence))
+    plt.xticks(ticks=np.linspace(0, 1, 6), labels=np.round(np.linspace(1, 0, 6), 2))
+    plt.yticks([])
     plt.title("Persistence Barcode")
     plt.savefig("test_result/" + output_file_name + "_barcode.png")
     # plt.show()
@@ -88,7 +98,7 @@ def plot_image_array(image_array, output_file_name="output"):
 def persistent_homology(image_data, output_file_name="output"):
     """Computes and visualizes the persistent homology for the given image data."""
     cc = gd.CubicalComplex(
-        dimensions=image_data.shape, top_dimensional_cells=image_data.flatten()
+        dimensions=image_data.shape, top_dimensional_cells=1 - image_data.flatten()
     )
     persistence = cc.persistence()
 
@@ -112,27 +122,95 @@ def convert_to_numpy_array(diagram):
     return np.array(diag_array)
 
 
-def create_simplicial_complex(image_array):
-    sc = gd.SimplexTree()
-    for i in range(len(image_array)):
-        for j in range(len(image_array[i])):
-            if image_array[i][j] > 0:
-                sc.insert([i, j])  # 単体の頂点を挿入
-    return sc
+def plot_diagram(a, b, name="ab"):
+    plt.clf()
+    plt.scatter(a[:, 0], a[:, 1], c="red", label="a")
+    plt.scatter(b[:, 0], b[:, 1], c="blue", label="b")
+    plt.title("Persistence Diagram")
+    plt.legend(bbox_to_anchor=(1, 0), loc="lower right", borderaxespad=1)
+    plt.xlabel("Birth")
+    plt.ylabel("Death")
+    plt.plot([0, 1], [0, 1], c="r")  # 対角線
 
+    bottleneck_distance = gd.bottleneck_distance(a, b)
+    cost, matchings = wasserstein_distance(a, b, matching=True)
+    message = "Bottleneck distance = " + "%.3f" % bottleneck_distance
+    plt.text(0.7, 0.3, message, ha="center", fontsize=10, color="black")
+    message = "Wasserstein distance = " + "%.3f" % cost
+    plt.text(0.7, 0.25, message, ha="center", fontsize=10, color="black")
 
-# Calling the function with the image_array to visualize the results
-# print("Image Array:")
-# print(image_array)
+    for i in range(len(a)):
+        plt.text(
+            a[i, 0] + 0.02,
+            a[i, 1],
+            f"({a[i, 0]:.1f}, {a[i, 1]:.1f})",
+            fontsize=8,
+            color="black",
+        )
+    for i in range(len(b)):
+        plt.text(
+            b[i, 0] + 0.02,
+            b[i, 1],
+            f"({b[i, 0]:.1f}, {b[i, 1]:.1f})",
+            fontsize=8,
+            color="black",
+        )
+
+    for match in matchings:
+        if match[0] != -1 and match[1] != -1:
+            plt.plot(
+                [a[match[0], 0], b[match[1], 0]],
+                [a[match[0], 1], b[match[1], 1]],
+                linestyle="--",
+                c="gray",
+            )
+        else:
+            if match[0] == -1:
+                x = (b[match[1], 0] + b[match[1], 1]) / 2
+                y = -x + b[match[1], 0] + b[match[1], 1]
+                plt.plot(
+                    [(b[match[1], 0]), x],
+                    [b[match[1], 1], y],
+                    linestyle="--",
+                    c="gray",
+                )
+                plt.text(
+                    x + 0.02,
+                    y,
+                    f"({x:.1f}, {y:.1f})",
+                    fontsize=8,
+                    color="black",
+                )
+            else:
+                x = (a[match[0], 0] + a[match[0], 1]) / 2
+                y = -x + a[match[0], 0] + a[match[0], 1]
+                plt.plot(
+                    [a[match[0], 0], x],
+                    [a[match[0], 1], y],
+                    linestyle="--",
+                    c="gray",
+                )
+                plt.text(
+                    x + 0.02,
+                    y,
+                    f"({x:.1f}, {y:.1f})",
+                    fontsize=8,
+                    color="black",
+                )
+    plt.savefig("test_result/" + name + "_diagram.png")
+    plt.clf()
+
 
 image_persistent = persistent_homology(image_array)
 convert_image_array = 1 - image_array
+conv_persistent = persistent_homology(image_array_2, "2")
 conv_persistent = persistent_homology(convert_image_array, "convert")
 
 # Compute the bottleneck distance between the two diagrams
 image_persistent_array = convert_to_numpy_array(image_persistent)
 conv_persistent_array = convert_to_numpy_array(conv_persistent)
-b_distance = gd.bottleneck_distance(image_persistent_array, conv_persistent_array)
-w_distance = wasserstein_distance(image_persistent_array, conv_persistent_array)
+plot_diagram(image_persistent_array, conv_persistent_array, "distance")
+# b_distance = gd.bottleneck_distance(image_persistent_array, conv_persistent_array)
+# w_distance = wasserstein_distance(image_persistent_array, conv_persistent_array)
 # print("Bottleneck Distance:%.2f " % b_distance)
 # print("Wasserstein Distance:%.2f " % w_distance)
